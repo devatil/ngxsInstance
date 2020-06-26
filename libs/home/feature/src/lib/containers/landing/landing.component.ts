3c3b26cab1fb985023949bdca4fd9f37b2ea4290
Atilla Altun <atilla.altun@ext.etiya.com>
2020-06-26 21:39:18 +0300

    adde landing page component

---
+++ libs/home/feature/src/lib/containers/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'agency-x-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/home/feature/src/lib/home-feature.module.ts
+++ libs/home/feature/src/lib/home-feature.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LandingComponent } from './containers/landing/landing.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: '', pathMatch: 'full', component: LandingComponent },
     ]),
   ],
+  declarations: [LandingComponent],
 })
 export class HomeFeatureModule {}
